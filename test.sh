a8797c2dfc5896fe371513132647413c4dd4ba49
zchwang <wangzch97@163.com>
2023-03-25 20:23:17 +0800

    update clip_receptor

--- test.sh
+++ test.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-rec=example/1tae/1tae_protein_noHETATM.pdbqt
-lig=example/1tae/1tae_NAD.pdbqt
-pose=example/1tae/1tae_NAD.pdbqt
-hetatm=example/1tae/HETATMs_pdbqt
+python=/d/Anaconda/envs/dgl/python
+
+rec=example/4tmn/4tmn_protein_atom_noHETATM.pdbqt
+lig=example/4tmn/4tmn_ligand.sdf
+pose=example/4tmn/4tmn_448.pdbqt
 
 
+$python test_clip.py $pose $rec $lig
 #$python distance_constraints.py  $pose $rec
 #$python outofbox_constraints.py $pose $rec
-python test_HETATM.py $pose $rec $hetatm
+#python test_HETATM.py $pose $rec $hetatm
 #$python test_HETATM.py $pose $rec
